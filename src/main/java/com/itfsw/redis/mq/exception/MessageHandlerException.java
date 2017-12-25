9660e48f6d9fa1de0217db54e89f3d4dbf4f145d
hewei <hewei@itfsw.com>
2017-12-25 10:49:59 +0800

    异常处理

--- src/main/java/com/itfsw/redis/mq/exception/MessageHandlerException.java
+++ src/main/java/com/itfsw/redis/mq/exception/MessageHandlerException.java
@@ -9,4 +9,22 @@ package com.itfsw.redis.mq.exception;
  * ---------------------------------------------------------------------------
  */
 public class MessageHandlerException extends RedisMqException {
+    public MessageHandlerException() {
+    }
+
+    public MessageHandlerException(String message) {
+        super(message);
+    }
+
+    public MessageHandlerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MessageHandlerException(Throwable cause) {
+        super(cause);
+    }
+
+    public MessageHandlerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
