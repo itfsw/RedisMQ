21b2ecc429bf28ba93894e3f739fed918d239872
hewei <hewei@itfsw.com>
2018-01-04 18:25:17 +0800

    减少没有任务时cpu,redis,网络的空转消耗

--- src/test/java/com/itfsw/redis/mq/support/producer/DefaultMessageSenderTest.java
+++ src/test/java/com/itfsw/redis/mq/support/producer/DefaultMessageSenderTest.java
@@ -1,13 +1,9 @@
 package com.itfsw.redis.mq.support.producer;
 
 import com.itfsw.redis.mq.MessageQueue;
-import com.itfsw.redis.mq.redis.RedisOperations;
-import com.itfsw.redis.mq.support.sender.IdWorker;
 import com.itfsw.redis.mq.utils.SpringTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Date;
 
 /**
  * ---------------------------------------------------------------------------
@@ -23,12 +19,6 @@ public class DefaultMessageSenderTest extends SpringTest {
 
     @Test
     public void send() throws Exception {
-        long time = new Date().getTime();
-       RedisOperations redisOperations = queue.redisOps();
-        for (int i = 0; i < 1000000; i ++){
-           IdWorker.generateId(redisOperations.time());
-        }
 
-        System.out.println(new Date().getTime() - time);
     }
 }
