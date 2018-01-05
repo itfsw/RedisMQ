97738540123295d246ac0d888cd401b3ee898dae
hewei <hewei@itfsw.com>
2018-01-05 17:06:42 +0800

    减少没有任务时cpu,redis,网络的空转消耗

--- src/main/java/com/itfsw/redis/mq/support/sender/IdWorker.java
+++ src/main/java/com/itfsw/redis/mq/support/sender/IdWorker.java
@@ -1,6 +1,7 @@
 package com.itfsw.redis.mq.support.sender;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.util.Random;
 
 /**
@@ -12,32 +13,27 @@ import java.util.Random;
  * ---------------------------------------------------------------------------
  */
 public class IdWorker {
-    /**
-     * 上次生成ID的时间截
-     */
-    private static long lastTimestamp = -1L;
-    /**
-     * 毫秒内序列
-     */
-    private static long sequence = 0L;
-    private static long workerId = 0L;
+    private static long lastTimestamp = -1L;    // 上次生成的时间戳
+    private static short sequence = 0;  // 毫秒内序列
+    private static long mac = 0L;   // 机器标识
+    private static short fix = 0;   // 修正位
 
     static {
-        long tmp = 0L;
+        // 获取机器标识
         try {
-            byte[] address = InetAddress.getLocalHost().getAddress();
-            // 取前32位
-            for (int i = 0; i < 4; i++) {
-                tmp = (tmp | address[i]) << 8;
+            byte[] macBytes = NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress();
+            for (byte bt : macBytes) {
+                mac = mac << 8 | bt;
             }
         } catch (Throwable e) {
-        } finally {
-            workerId = Math.abs(tmp << 32 | new Random().nextInt());
+            mac = new Random().nextLong() >>> 16;
         }
+        // 修正位
+        fix = (short) (Math.abs(new Random().nextInt(Short.MAX_VALUE)));
     }
 
     /**
-     * 生成有序Id
+     * 生成有序Id（最大32位）
      * @param millis
      * @return
      */
@@ -46,8 +42,7 @@ public class IdWorker {
 
         //如果服务器时间有问题(时钟后退) 报错。
         if (timestamp < lastTimestamp) {
-            // 更新worker id
-            workerId = workerId >> 32 << 32 | new Random().nextInt();
+            fixed();
         }
 
         //如果上次生成时间和当前时间相同,在同一毫秒内
@@ -55,22 +50,36 @@ public class IdWorker {
             //sequence自增
             sequence++;
             //判断是否溢出
-            if (sequence == Long.MAX_VALUE) {
+            if (sequence == Short.MAX_VALUE) {
                 // 阻塞1毫秒(不可能进入该流程)
                 try {
                     Thread.sleep(1);
                 } catch (InterruptedException e) {
-                    throw new RuntimeException(String.format("Thread InterruptedException.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp), e);
+                    fixed();
                 }
                 sequence = 0;
                 timestamp++;
             }
         } else {
-            sequence = 0L; //如果和上次生成时间不同,重置sequence，就是下一毫秒开始，sequence计数重新从0开始累加
+            sequence = 0; //如果和上次生成时间不同,重置sequence，就是下一毫秒开始，sequence计数重新从0开始累加
         }
         lastTimestamp = timestamp;
 
-        return String.format("%s-%s-%s", Long.toString(lastTimestamp, Character.MAX_RADIX), Long.toString(sequence, Character.MAX_RADIX), Long.toString(workerId, Character.MAX_RADIX));
+        return String.format("%s-%s-%s-%s",
+                Long.toString(lastTimestamp, Character.MAX_RADIX),
+                Long.toString(sequence, Character.MAX_RADIX),
+                Long.toString(mac, Character.MAX_RADIX),
+                Long.toString(fix, Character.MAX_RADIX)
+        );
+    }
+
+    /**
+     * 修正
+     */
+    private static void fixed() {
+        if (fix++ == Short.MAX_VALUE) {
+            fix = 0;
+        }
     }
 
     /**
